ee8c95efe7de167c7dcb0a8d6c19e72c3cb46490
adri6412 <32240572+adri6412@users.noreply.github.com>
2024-09-01 12:40:04 +0200

    Update comfy.sh

--- comfy.sh
+++ comfy.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
+git clone https://github.com/comfyanonymous/ComfyUI.git
+cd ComfyUI/custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+cd ..
+pip install -r requirements
